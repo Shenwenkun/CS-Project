923f8168019530f9b03b05cac2d1b7addb6c58ec
申文琨 <112930167+Shenwenkun@users.noreply.github.com>
2024-05-18 17:55:14 +0800

    Add files via upload

---
+++ DataPath/MUX_F1.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/05/18 17:29:31
+// Design Name: 
+// Module Name: MUX_F1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUX_F1(
+input [31:0] rdata1_i,ALUResult_i,data_i,
+input [1:0] ForwardA,
+output reg [31:0] rdata1_o
+    );
+    
+    always@*begin
+       case(ForwardA)
+       2'b00: rdata1_o=rdata1_i;
+       2'b01:rdata1_o=data_i;
+       2'b10: rdata1_o=ALUResult_i;
+       endcase
+    end
+    
+endmodule
