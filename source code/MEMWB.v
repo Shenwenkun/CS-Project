a89badf7f9b6cef07f94edb172cc170fac1fd31c
Spider-Man2023 <112930206+Spider-Man2023@users.noreply.github.com>
2024-05-20 01:10:51 +0800

    Update MEMWB.v
    Let this module calculate the final data written back to registers in module Decoder.

--- source code/MEMWB.v
+++ source code/MEMWB.v
@@ -24,37 +24,38 @@ module MEMWB(
 input clk,rst_n,RegWrite_i,MemtoReg_i,
 input [31:0]data_i,ALUResult_i,
 input [4:0]rd_i,
-output reg RegWrite_o,MemtoReg_o,
-output reg [31:0]data_o,ALUResult_o,
+output reg RegWrite_o,
+output reg [31:0]wdata_o,
 output reg [4:0]rd_o
     );
-    reg RegWrite,MemtoReg;
-    reg [31:0]data,ALUResult;
+    reg RegWrite;
+    reg [31:0]wdata;
     reg [4:0]rd;
     always@(posedge clk)begin
         if (rst_n==1'b1)begin
-            RegWrite_o<=1'b0;MemtoReg_o<=1'b0;
-            data_o<=32'b0;ALUResult_o<=32'b0;
+            RegWrite_o<=1'b0;
+            wdata_o<=32'b0;
+            rd_o<=5'b0;
         end
         else begin
             RegWrite<=RegWrite_i;
-            MemtoReg<=MemtoReg_i;
-            data<=data_i;
-            ALUResult<=ALUResult_i;
             rd<=rd_i;
+            case(MemtoReg_i)
+                1'b1:wdata<=data_i;
+                default:wdata<=ALUResult_i;
+            endcase
         end
     end
     
     always@(negedge clk)begin
         if (rst_n==1'b1)begin
-            RegWrite_o<=1'b0;MemtoReg_o<=1'b0;
-            data_o<=32'b0;ALUResult_o<=32'b0;
+            RegWrite_o<=1'b0;
+            wdata_o<=32'b0;
+            rd_o<=5'b0;
         end
         else begin
             RegWrite_o<=RegWrite;
-            MemtoReg_o<=MemtoReg;
-            data_o<=data;
-            ALUResult_o<=ALUResult;
+            wdata_o<=wdata;
             rd_o<=rd;
         end
     end
