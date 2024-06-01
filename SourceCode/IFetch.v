29a2dc5d2fbcbbd60aa9af1806f268e3bbc5f2d1
mingqikeliu <112930594+mingqikeliu@users.noreply.github.com>
2024-06-01 23:59:57 +0800

    Add files via upload
    uart mode

--- SourceCode/IFetch.v
+++ SourceCode/IFetch.v
@@ -5,14 +5,30 @@ input clk, rst_n,
 input PCSrc_i,
 input [13:0] addr_i,
 output [31:0] instr_o,
-output reg [13:0] addr_o
+output reg [13:0] addr_o,
+
+input upg_rst_i, // UPG reset (Active High)
+input upg_clk_i, // UPG clock (10MHz)
+input upg_wen_i, // UPG write enable
+input[13:0] upg_addr_i, // UPG write address
+input[31:0] upg_data_i, // UPG write data
+input upg_done_i // 1 if program finished
     );
-    reg [13:0] pc;
+    reg [13:0] pc = 14'b0;
     reg PCSrc;
-    initial begin
-        pc=14'h0;
-    end 
-    ROM uram(.clka(~clk),.addra((PCSrc==1'b1)?addr_i:pc),.douta(instr_o));
+
+    /* if kickOff is 1 means CPU work on normal mode, otherwise CPU work on Uart communicationmode*/
+   wire kickOff = upg_rst_i | (~upg_rst_i & upg_done_i );
+
+//   use a RAM, modify the name
+    RAM_IF instmem (
+    .clka (kickOff ? ~clk : ~upg_clk_i ),
+    .wea (kickOff ? 1'b0 : upg_wen_i ),
+    .addra (kickOff ? pc : upg_addr_i ),
+    .dina (kickOff ? 32'h00000000 : upg_data_i ),
+    .douta (instr_o)
+    );
+
     always@(posedge clk)begin
         PCSrc<=PCSrc_i;
     end
