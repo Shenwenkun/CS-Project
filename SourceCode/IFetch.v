ee5f43c5afeb3dec6f32c0779b8e0c3154f74cb2
申文琨 <112930167+Shenwenkun@users.noreply.github.com>
2024-06-01 22:13:31 +0800

    Add files via upload

--- SourceCode/IFetch.v
+++ SourceCode/IFetch.v
@@ -4,61 +4,24 @@ module IFetch(
 input clk, rst_n,
 input PCSrc_i,
 input [13:0] addr_i,
-output reg [31:0] instr_o,
+output [31:0] instr_o,
 output reg [13:0] addr_o
     );
-    reg [13:0] current_pc;
-    wire [31:0] instr;
-    reg [13:0]addr;
+    reg [13:0] pc;
+    reg PCSrc;
     initial begin
-        current_pc=14'b0;
-    end
-//    always @(posedge clk) begin
-//        if(rst_n==1'b1)begin
-//            pc <= 14'b00000000000000; //maybe this should be changed!!!!!
-//        end
-//        else begin
-//            case(PCSrc_i)
-//                1'b1:begin
-//                    pc <= addr_i;
-//                end
-//                1'b0:begin
-//                    pc <= pc + 4;
-//                end
-//            endcase
-//        end
-//    end
-//    always @(negedge clk) begin
-//        if(rst_n==1'b1)begin
-//            pc <= 14'b00000000000000; //maybe this should be changed!!!!!
-//        end else
-//        begin
-//            addr_o <= pc;
-//            instr_o <= instr;
-//        end
-//    end
-//    assign pc=(rst_n==1'b1)? 1'b0:(PCSrc==1'b1)? addr:current_pc;
-    
+        pc=14'h0;
+    end 
+    ROM uram(.clka(~clk),.addra((PCSrc==1'b1)?addr_i:pc),.douta(instr_o));
     always@(posedge clk)begin
+        PCSrc<=PCSrc_i;
+    end
+    always@(negedge clk or posedge rst_n)begin
         if(rst_n==1'b1)begin
-            addr<=14'b0;
-            current_pc<=14'b0;
+            pc<=14'b00000000000000;
         end else begin
-            addr<=addr_i;
-            current_pc<=(PCSrc_i==1'b1)? addr_i:(current_pc+4);
+            addr_o<=(PCSrc==1'b1)?addr_i:pc;
+            pc<=(PCSrc==1'b1)?addr_i:pc+1;
         end
     end
-    ROM uram(.clka(clk),.addra(current_pc),.douta(instr));
-    always@(negedge clk)begin
-        if(rst_n==1'b1)begin
-            addr<=14'b0;
-            current_pc<=14'b0;
-            instr_o<=0;
-            addr_o<=0;
-        end else begin
-            instr_o<=instr;
-            addr_o<=current_pc;
-        end
-    end
-    
 endmodule
